747574543245842426893faf42f045939437dea4
amyusup <amyusup26@gmail.com>
2021-02-11 15:14:29 +0700

    setup hosting

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("*", cors());
 app.get("/", (req, res) => {
-  res.send("server online");
+  res.send("server online.");
 });
 
 app.use("/api/v1", routeNavigator)
